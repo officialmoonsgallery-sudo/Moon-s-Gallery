90caa38b1f6e6b9b8b40e770428091bd1f7f16da
officialmoonsgallery-sudo <officialmoonsgallery@gmail.com>
2026-08-07 14:15:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwIlD23H2tor2kDeYmwnuNL5ih52ry6eTM5CMhhjWdC-9SRrBh-BOVSvBy64_QmsQ2BFg/exec";
 function deliveryCharge(area){
     return area==="Dhaka"?80:150;
 }
