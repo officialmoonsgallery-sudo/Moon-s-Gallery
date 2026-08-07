569fee655bd3abef55021a299a43525f8d9dc3a7
officialmoonsgallery-sudo <officialmoonsgallery@gmail.com>
2026-08-07 10:43:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function deliveryCharge(area){
+    return area==="Dhaka"?80:150;
+}
 
+function calculateTotal(price, qty, area){
+    return (price * qty) + deliveryCharge(area);
+}
+
+function buyNow(product,color,price){
+    localStorage.setItem("product",product);
+    localStorage.setItem("color",color);
+    localStorage.setItem("price",price);
+
+    window.location.href="checkout.html";
+      }
